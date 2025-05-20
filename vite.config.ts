81d33ce524cc6a2e37444f583f1329e2221b5e38
dpiercey <dpiercey@ebay.com>
2025-05-20 16:18:52 -0700

    fix: grid changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import marko from "@marko/run/vite";
 import markodown from "./src/util/markodown";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { patchCssModules } from "vite-css-modules";
 
 export default defineConfig({
   plugins: [
+    patchCssModules(),
     markodown(),
     marko({
       trailingSlashes: "RedirectWithout",
