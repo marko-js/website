3470bee0354858e9c0bf8ae86531aab11a956470
dpiercey <dpiercey@ebay.com>
2025-07-10 11:03:03 -0700

    fix: support safari

---
+++ src/util/stream-to-iterable.ts
@@ -0,0 +1,14 @@
+export async function* streamToIterable<T>(
+  stream: ReadableStream<T>
+): AsyncIterable<T> {
+  const reader = stream.getReader();
+  try {
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) break;
+      yield value!;
+    }
+  } finally {
+    reader.releaseLock();
+  }
+}
