e540ebf56253217e4054e77d975f17ee1fe4913a
dpiercey <dpiercey@ebay.com>
2025-11-09 14:10:30 -0700

    fix: switch to onigurma shiki

--- src/util/shiki.ts
+++ src/util/shiki.ts
@@ -1,17 +1,17 @@
 import { createHighlighterCore } from "shiki/core";
-import { createJavaScriptRawEngine } from "shiki/engine/javascript";
+import { createOnigurumaEngine } from "shiki/engine/oniguruma";
 
-import css from "@shikijs/langs-precompiled/css";
-import html from "@shikijs/langs-precompiled/html";
-import js from "@shikijs/langs-precompiled/javascript";
-import json from "@shikijs/langs-precompiled/json";
-import marko from "@shikijs/langs-precompiled/marko";
-import sh from "@shikijs/langs-precompiled/shellscript";
-import ts from "@shikijs/langs-precompiled/typescript";
+import css from "@shikijs/langs/css";
+import html from "@shikijs/langs/html";
+import js from "@shikijs/langs/javascript";
+import json from "@shikijs/langs/json";
+import marko from "@shikijs/langs/marko";
+import sh from "@shikijs/langs/shellscript";
+import ts from "@shikijs/langs/typescript";
 import { dark, light } from "./shiki-theme";
 
 export default await createHighlighterCore({
-  engine: createJavaScriptRawEngine(),
+  engine: createOnigurumaEngine(import("shiki/wasm")),
   themes: [dark, light],
   langs: [css, html, js, json, marko, sh, ts],
 });
